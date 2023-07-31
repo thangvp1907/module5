beb900eff7b2e9d3da6d2a488333c51a9f0faef9
thangvp1907 <thangvp1999@gmail.com>
2023-07-31 11:36:51 +0700

    update

--- bai_3/thuc_hanh/angular-hackernews-app/karma.conf.js
+++ bai_3/thuc_hanh/angular-hackernews-app/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/casestudy'),
+      dir: require('path').join(__dirname, './coverage/angular-hackernews-app'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
